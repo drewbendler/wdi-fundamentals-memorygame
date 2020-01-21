a1a74ce254ef1411cd9f7474998c719a0ecdab47
drewbendler <59902662+drewbendler@users.noreply.github.com>
2020-01-21 01:37:02 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,27 +1,60 @@
 console.log("Up and Running!");
 
 
-const cards = ["queen", "queen", "king", "king" ];
 
-const cardsInPlay = []
+const cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+
+}
+];
+
+
+ const cardsInPlay = []
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You got a match!");
+		alert("You got a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 function flipCard(cardId){
 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
+
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+
 
 return;
 
+
+
+
 };
+
 
 flipCard(0);
 flipCard(2);
@@ -31,6 +64,7 @@ checkForMatch();
 const cardOne = cards[0];
 
 const cardTwo = cards[2];
+
 
 
 
